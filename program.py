2e0835dbe3e2896ebd75e86d667299dc0bbb325e
mrale <mr.alex.pan@list.ru>
2019-10-07 22:38:49 +0300

    Четвертый коммит

--- program.py
+++ program.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     print('online redactor')
     print("first git file")
     print('hi boy')
+    print('main branch')
     #    app = QApplication(sys.argv)
     #    ex = Example()
     #    ex.show()
