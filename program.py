62f09f496930cab3fc01dd69ef76db486e680e5f
mrale <mr.alex.pan@list.ru>
2019-10-07 22:46:35 +0300

    седьмой коммит создал ветку четвертую

--- program.py
+++ program.py
@@ -1,10 +1,9 @@
 
 
 if __name__ == '__main__':
-    print('online redactor')
     print("first git file")
-    print('hi boy')
     print('main branch')
+    print('fourth branch')
     #    app = QApplication(sys.argv)
     #    ex = Example()
     #    ex.show()
