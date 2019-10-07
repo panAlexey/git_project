345af010b147c8bb2399e4969fad68b4ac1ce577
mrale <mr.alex.pan@list.ru>
2019-10-07 22:28:54 +0300

    Наш третий коммит, но по отдельной ветке

--- program.py
+++ program.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     print('online redactor')
     print("first git file")
     print('hi boy')
+    print("new branch, i see you")
     #    app = QApplication(sys.argv)
     #    ex = Example()
     #    ex.show()
